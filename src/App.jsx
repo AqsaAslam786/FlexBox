f087a8c0388a8f05789a36ebbdcb7d02f54469f4
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-09-27 08:27:55 +0500

    cos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Costa_Rica_Flag from "./compounent/Costa_Rica_Flag"
 import France_Flag from "./compounent/France_Flag"
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
     <div className="m-7 w-400">
       < France_Flag />
       </div>
+      <h1>Costa_Rica_Flag </h1>
+      < Costa_Rica_Flag />
+     
     </>
   )
 }
---
+++ src/compounent/Costa_Rica_Flag.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Costa_Rica_Flag() {
+  return (
+    <div className=" h-70 w-120 ">
+        <div className="bg-blue-700 w-120 h-1/6 "></div>
+        <div className="bg-white w-120 h-1/6 "></div>
+        <div className="bg-red-600 w-120 h-2/6 "></div>
+        <div className="bg-white w-120 h-1/6 "></div>
+        <div className="bg-blue-700 w-120 h-1/6 "></div>
+      
+      </div>
+  )
+}
