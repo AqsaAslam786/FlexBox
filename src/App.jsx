91040f1d9ddb9a59185b20ce918bc3afaa55d40a
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-09-27 08:06:54 +0500

    france

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
+import France_Flag from "./compounent/France_Flag"
 
 function App() {
 
 
   return (
     <>
-      <div className="flex m-7 h-[500px] max-w-[600px] bg-amber-600">
-        <div className="h-24 w-24 bg-green-800">Green</div>
-        <div className="h-24 w-24 bg-red-800 ">Red</div>
-        <div className="h-24 w-24 bg-blue-800 ">Blue</div>
-        <div className="h-24 w-24 bg-purple-800 ">Purple</div>
-      
+    <div className="m-7 w-400">
+      < France_Flag />
       </div>
     </>
   )
--- src/compounent/France_Flag.jsx
+++ src/compounent/France_Flag.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function France_Flag() {
   return (
-    <div>
+    <div className="flex flex-wrap flex-row">
+        <div className="h-110 bg-blue-950 w-50  "></div>
+        <div className="h-110 bg-white w-50  "></div>
+        <div className="h-110 bg-red-800 w-50 order-3"></div>
+        
+        
       
-    </div>
+      </div>
   )
 }
