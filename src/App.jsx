2b8cf621ee7ff544e771d0d8e52ad337598466e3
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-09-27 07:16:38 +0500

    flag

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-      <div>Flag </div>
+      <div className="flex m-7 h-[500px] max-w-[600px] bg-amber-600">
+        <div className="h-24 w-24 bg-green-800">Green</div>
+        <div className="h-24 w-24 bg-red-800 ">Red</div>
+        <div className="h-24 w-24 bg-blue-800 ">Blue</div>
+        <div className="h-24 w-24 bg-purple-800 ">Purple</div>
+      
+      </div>
     </>
   )
 }
